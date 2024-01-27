ef64e5e4e9ab5a31066fcb265cdffc73d26e0006
ooooose <yuuki.oose1990@gmail.com>
2024-01-27 22:58:12 +0900

    Add 認証次第で条件分岐

--- src/components/layouts/Header/Header.tsx
+++ src/components/layouts/Header/Header.tsx
@@ -1,16 +1,19 @@
+'use client'
+
 import React from 'react'
 import Link from 'next/link'
 import { Navigation } from '@/components/elements/Navigation/Navigation'
+import { useSession } from 'next-auth/react'
+import { Login } from '@/features/auth/components/Login'
 
 const Header = () => {
+  const { status } = useSession()
   return (
     <div className='py-8 px-3 sm:px-10 text-4xl flex items-center justify-between'>
       <div className='font-medium'>
         <Link href='/'>画HACK</Link>
       </div>
-      <div className='flex gap-2'>
-        <Navigation />
-      </div>
+      <div className='flex gap-2'>{status === 'authenticated' ? <Navigation /> : <Login />}</div>
     </div>
   )
 }
