5bcc9292ee7bf5f4fe8f32c3fa639ec117c0afc8
ooooose <yuuki.oose1990@gmail.com>
2024-01-28 17:08:54 +0900

    Fix ダイアログが閉じないバグを改善

--- src/features/auth/components/Login.tsx
+++ src/features/auth/components/Login.tsx
@@ -4,16 +4,21 @@ import Link from 'next/link'
 import { Modal } from '@/components/elements'
 import { Button } from '@/components/ui/button'
 import { signIn } from 'next-auth/react'
+import { DialogClose } from '@/components/ui/dialog'
 
 export const Login = () => {
   return (
     <Modal text='はじめる' description='ログインしますか？'>
-      <Button variant='outline' onClick={() => signIn('google', {}, { prompt: 'login' })}>
-        Googleアカウントでログイン
-      </Button>
-      <Button variant='outline' className='bg-gray-100' asChild>
-        <Link href='/login'>メールでログイン</Link>
-      </Button>
+      <DialogClose asChild>
+        <Button variant='outline' onClick={() => signIn('google', {}, { prompt: 'login' })}>
+          Googleアカウントでログイン
+        </Button>
+      </DialogClose>
+      <DialogClose asChild>
+        <Button variant='outline' className='bg-gray-100' asChild>
+          <Link href='/login'>メールでログイン</Link>
+        </Button>
+      </DialogClose>
     </Modal>
   )
 }
