0142c409ff4d0c642d05d94096984edfe49a1cb3
ooooose <yuuki.oose1990@gmail.com>
2024-02-16 21:10:21 +0900

    Add Headの追加（失敗）

--- src/components/layouts/Layout/MainLayout.tsx
+++ src/components/layouts/Layout/MainLayout.tsx
@@ -8,13 +8,13 @@ type MainLayoutProps = {
 
 const MainLayout = ({ children }: MainLayoutProps) => {
   return (
-    <>
-      <div className='md:w-2/3 w-full mx-auto select-none'>
+    <div className='select-none'>
+      <div className='md:w-2/3 w-full mx-auto'>
         <Header />
         <div className='flex flex-col items-center justify-between h-screen'>{children}</div>
       </div>
       <Footer />
-    </>
+    </div>
   )
 }
 
