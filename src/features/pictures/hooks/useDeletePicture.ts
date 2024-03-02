849da13efba3308c11a42974ce7299ee6a4209bf
ooooose <yuuki.oose1990@gmail.com>
2024-03-02 17:43:59 +0900

    Add useCallbackの適用

--- src/features/pictures/hooks/useDeletePicture.ts
+++ src/features/pictures/hooks/useDeletePicture.ts
@@ -1,3 +1,4 @@
+import { useCallback } from 'react'
 import { toast } from 'sonner'
 
 import { deletePicture } from '../api'
@@ -6,22 +7,25 @@ import { useFetchPictures } from './useFetchPictures'
 
 export const useDeletePicture = () => {
   const { mutate } = useFetchPictures()
-  const handleDeletePicture = (pictureId: string, image: string) => {
-    const params = {
-      id: pictureId,
-      image: image,
-    }
-    try {
-      deletePicture(params).then((res) => {
-        if (res.status === 200) {
-          mutate()
-          toast('絵を削除しました', { position: 'top-center' })
-        }
-      })
-    } catch (err) {
-      console.log(err)
-    }
-  }
+  const handleDeletePicture = useCallback(
+    (pictureId: string, image: string) => {
+      const params = {
+        id: pictureId,
+        image: image,
+      }
+      try {
+        deletePicture(params).then((res) => {
+          if (res.status === 200) {
+            mutate()
+            toast('絵を削除しました', { position: 'top-center' })
+          }
+        })
+      } catch (err) {
+        console.log(err)
+      }
+    },
+    [mutate],
+  )
 
   return { handleDeletePicture }
 }
