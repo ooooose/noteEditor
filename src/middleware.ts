a69f991adb7383ff2325fbdbc49489126930ca7e
ooooose <yuuki.oose1990@gmail.com>
2024-06-02 10:29:17 +0900

    Fix ミドルウェアの修正

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,37 @@
+import { NextResponse } from 'next/server'
+import { getToken } from 'next-auth/jwt'
 import { withAuth } from 'next-auth/middleware'
 
-export default withAuth({
-  // リダイレクトページ
-  pages: {
-    signIn: '/login',
-  },
-})
+import type { NextRequest } from 'next/server'
+import type { JWT } from 'next-auth/jwt'
+
+interface NextRequestWithAuth extends NextRequest {
+  nextauth: {
+    token: JWT | null
+    session: object | null
+  }
+}
+
+export default async function middleware(req: NextRequestWithAuth, ev: any) {
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
+
+  // 認証ミドルウェアの設定
+  const authMiddleware = withAuth({
+    pages: {
+      signIn: '/login',
+    },
+  })
+
+  // authMiddlewareを呼び出し
+  const response = await authMiddleware(req, ev)
+
+  // セッションがあり、ログインページにアクセスした場合ホームページにリダイレクト
+  if (token && req.nextUrl.pathname === '/login') {
+    return NextResponse.redirect(new URL('/', req.url))
+  }
+
+  return response
+}
 
 export const config = {
   matcher: ['/timeline', '/me', '/canvas'],
