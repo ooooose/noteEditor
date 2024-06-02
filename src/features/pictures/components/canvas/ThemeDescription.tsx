a1c0eaff365bf675777ed10b702bfba4c9d01e29
ooooose <yuuki.oose1990@gmail.com>
2024-06-02 10:46:00 +0900

    Add テーマ説明モーダルに閉じるボタンを作成

--- src/features/pictures/components/canvas/ThemeDescription.tsx
+++ src/features/pictures/components/canvas/ThemeDescription.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/elements/Button'
 import {
   Dialog,
   DialogContent,
@@ -5,6 +6,7 @@ import {
   DialogTitle,
   DialogTrigger,
   DialogDescription,
+  DialogClose,
 } from '@/components/ui/dialog'
 
 export const ThemeDescription = () => {
@@ -19,11 +21,16 @@ export const ThemeDescription = () => {
         <DialogHeader>
           <DialogTitle>テーマとは？</DialogTitle>
         </DialogHeader>
-        <DialogDescription>
+        <DialogDescription className='mb-5'>
           設定されたテーマに沿って絵を書いてみましょう！
           <br />
           ランダムに設定されますが、選択し直すことも作成することも可能です！
         </DialogDescription>
+        <DialogClose asChild>
+          <Button className='bg-gray-100' variant='outline'>
+            閉じる
+          </Button>
+        </DialogClose>
       </DialogContent>
     </Dialog>
   )
