709b1e6eb2219092d3d230a8496ea2d2986b84d8
ooooose <yuuki.oose1990@gmail.com>
2024-12-15 14:04:03 +0900

    Fix ローディング画面の修正

--- src/features/pictures/components/LoadingPictures.tsx
+++ src/features/pictures/components/LoadingPictures.tsx
@@ -3,7 +3,7 @@ import { SkeletonCard } from '@/components/elements/Skeleton/SkeletonCard'
 const LoadingPictures = () => {
   return (
     <div>
-      <div className='grid w-[800px] grid-cols-3 grid-rows-2 gap-1'>
+      <div className='mt-10 grid grid-cols-3 grid-rows-2 gap-10'>
         {[...Array(6)].map((_, i) => {
           return <SkeletonCard key={i} />
         })}
