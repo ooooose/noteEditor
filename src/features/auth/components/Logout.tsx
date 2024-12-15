8a1c65f163c76bfe9a02737054297526898b7adb
ooooose <yuuki.oose1990@gmail.com>
2024-12-16 08:03:36 +0900

    Fix 認証ボタンの整理

--- src/features/auth/components/Logout.tsx
+++ src/features/auth/components/Logout.tsx
@@ -1,37 +1,14 @@
 'use client'
 
-import { useRouter } from 'next/navigation'
 import { signOut } from 'next-auth/react'
 import React from 'react'
 
-import { Modal } from '@/components/elements'
 import { Button } from '@/components/ui/button'
-import { DialogClose } from '@/components/ui/dialog'
 
 export const Logout = () => {
-  const router = useRouter()
   return (
-    <Modal description='ログアウトしますか？' text='ログアウト'>
-      <Button
-        onClick={() => {
-          signOut()
-            .then(() => {
-              console.log('Sign-out successful')
-              router.push('/')
-            })
-            .catch((error) => {
-              console.error('Sign-out error:', error)
-            })
-        }}
-        variant='outline'
-      >
-        ログアウト
-      </Button>
-      <DialogClose asChild>
-        <Button className='bg-gray-100' variant='outline'>
-          キャンセル
-        </Button>
-      </DialogClose>
-    </Modal>
+    <Button onClick={() => signOut()} variant='outline'>
+      ログアウト
+    </Button>
   )
 }
