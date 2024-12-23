ae7cd1229898b2a76730f5c03e8c927dff23ab04
ooooose <yuuki.oose1990@gmail.com>
2024-12-24 06:45:56 +0900

    Add プライバシーポリシーの制定

--- src/app/terms/page.tsx
+++ src/app/terms/page.tsx
@@ -1,14 +1,32 @@
+'use client'
+
+import { useState } from 'react'
+
 import MainLayout from '@/components/layouts/Layout/MainLayout'
-import { TermsOfService } from '@/components/layouts/Terms/terms-of-service'
+import { TermsOfService, PrivacyPolicy } from '@/components/layouts/Terms'
 
 export default function Terms() {
+  const [activeTab, setActiveTab] = useState(0)
+  const tabs = [
+    { title: '利用規約', content: <TermsOfService /> },
+    { title: 'プライバシーポリシー', content: <PrivacyPolicy /> },
+  ]
+
   return (
     <MainLayout>
       <div className='w-[700px] py-8'>
         <section className='mb-8 border-b-2 py-2 pt-8'>
-          <h2 className='text-2xl'>利用規約</h2>
+          {tabs.map((tab, index) => (
+            <button
+              className={`${activeTab === index ? 'text-xl' : 'text-xl text-gray-500'} mr-4`}
+              key={index}
+              onClick={() => setActiveTab(index)}
+            >
+              {tab.title}
+            </button>
+          ))}
         </section>
-        <TermsOfService />
+        {tabs[activeTab].content}
       </div>
     </MainLayout>
   )
