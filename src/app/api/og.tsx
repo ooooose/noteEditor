1a528388573da4996f2c0878c5044282b484e12a
ooooose <yuuki.oose1990@gmail.com>
2025-08-13 22:28:15 +0900

    Fix ogp関連のAPI作成

--- src/app/api/og.tsx
+++ src/app/api/og.tsx
@@ -1,45 +1,29 @@
-import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3'
-import Image from 'next/image'
+import { Deserializer } from 'jsonapi-serializer'
 import { ImageResponse } from 'next/og'
+
+import { Picture as PictureComponent } from '@/features/pictures/components/Picture'
+import { Picture } from '@/features/pictures/types'
+import { apiClient } from '@/lib/api/api-client'
+
+import type { DeserializerOptions } from 'jsonapi-serializer'
 
 export const runtime = 'edge'
 
+const deserializerOptions: DeserializerOptions = {
+  keyForAttribute: 'camelCase',
+}
+
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url)
-  const fileName = searchParams.get('fileName')
-  if (!fileName) {
-    return new Response('fileName is required', { status: 400 })
+  const pictureUid = searchParams.get('pictureId')
+  if (!pictureUid) {
+    return new Response('pictureId is required', { status: 400 })
   }
 
-  const {
-    CLOUDFLARE_ACCESS_KEY_ID,
-    CLOUDFLARE_ENDPOINT,
-    CLOUDFLARE_ACCESS_KEY,
-    REGION,
-    BUCKET_NAME,
-  } = process.env
-
-  const s3Client = new S3Client({
-    region: REGION,
-    endpoint: CLOUDFLARE_ENDPOINT as string,
-    credentials: {
-      accessKeyId: CLOUDFLARE_ACCESS_KEY_ID || '',
-      secretAccessKey: CLOUDFLARE_ACCESS_KEY || '',
-    },
-  })
-
   try {
-    const command = new GetObjectCommand({
-      Bucket: BUCKET_NAME,
-      Key: fileName,
-    })
-    const response = await s3Client.send(command)
-    if (!response.Body) {
-      return new Response('OGP fetch error: Body is undefined', { status: 500 })
-    }
-    const arrayBuffer = await response.Body.transformToByteArray()
-    const imageData = Buffer.from(arrayBuffer)
-
+    const response = await apiClient.get(`/api/v1/pictures/${pictureUid}`)
+    const deserializer = new Deserializer(deserializerOptions)
+    const picture: Picture = await deserializer.deserialize(response)
     return new ImageResponse(
       (
         <div
@@ -52,12 +36,10 @@ export async function GET(req: Request) {
             background: '#fff',
           }}
         >
-          <Image
-            alt='OGP'
-            height={630}
-            src={`data:${response.ContentType};base64,${imageData.toString('base64')}`}
-            style={{ objectFit: 'cover' }}
-            width={1200}
+          <PictureComponent
+            author={picture.user.name}
+            frameId={picture.frameId}
+            src={picture.imageUrl}
           />
         </div>
       ),
