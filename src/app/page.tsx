350a5d05589bbfc23e0cefbd0ecaa26ef767e0c3
ooooose <yuuki.oose1990@gmail.com>
2025-08-13 23:06:42 +0900

    Fix OGP設定

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
-
 import { Palette, Sparkles, Users } from 'lucide-react'
+import { Metadata } from 'next'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useSession } from 'next-auth/react'
@@ -20,6 +20,26 @@ import { Skeleton } from '@/components/ui/skeleton'
 import { Login } from '@/features/auth/components'
 import { TopPictures } from '@/features/top/components/top-pictures'
 import { TopUsers } from '@/features/top/components/top-users'
+import { baseURL } from '@/lib/constants/env'
+
+type Props = {
+  searchParams: { [key: string]: string | undefined }
+}
+
+export function generateMetadata({ searchParams }: Props): Metadata {
+  const pictureId = searchParams?.pictureId
+  return {
+    metadataBase: new URL('https://gahack.net'),
+    title: '画HACK',
+    description: 'あなたらしい絵を描くアプリ',
+    twitter: {
+      title: '画HACK',
+      description: 'あなたらしい絵を描くアプリ',
+      card: 'summary_large_image',
+      images: [`${baseURL}/og?pictureId=${pictureId}`],
+    },
+  }
+}
 
 export default function Home() {
   const { status } = useSession()
