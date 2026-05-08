6d87bed10235100bd074f12747fc074fdf11d3e5
ooooose <yuuki.oose1990@gmail.com>
2026-05-08 22:59:30 +0900

    [add]素材追加

--- src/components/layouts/Header/Header.tsx
+++ src/components/layouts/Header/Header.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
@@ -10,6 +11,7 @@ const Header = () => {
     <header className='mx-auto w-full border-b border-white/40 bg-white/60 backdrop-blur-sm'>
       <div className='flex h-16 items-center justify-between sm:h-20'>
         <div className='flex items-center gap-2'>
+          <Image alt='Main Character' height={40} src='/main-character.png' width={40} />
           <Link href='/'>
             <span className='text-3xl font-bold tracking-tight sm:text-4xl'>
               <span className='text-red-500'>画</span>
